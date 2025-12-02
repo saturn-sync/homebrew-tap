6c90b2571f31e1af3040d0622083b34df746ae69
Markus Meyer <hydrantanderwand@gmail.com>
2025-12-02 16:25:37 +0000

    update Saturn to v0.46.1

--- Formula/saturn.rb
+++ Formula/saturn.rb
@@ -1,11 +1,11 @@
 class Saturn < Formula
   desc "Create, modify or delete files across many repositories."
   homepage "https://codeberg.org/saturn-sync/saturn"
-  url "https://codeberg.org/saturn-sync/saturn/archive/v0.46.0.tar.gz"
-  sha256 "6d811ff7fec00182931e010e0ccd16f41d12eafcf607447d0cacb8d4c3d77499"
+  url "https://codeberg.org/saturn-sync/saturn/archive/v0.46.1.tar.gz"
+  sha256 "2b1a8283f0e68a39d4a9c0a9ad367fbd6739780a088d997b7ed363b3a0bb1f17"
   license "AGPL-3.0"
   head "https://codeberg.org/saturn-sync/saturn.git", branch: "main"
-  version "0.46.0"
+  version "0.46.1"
 
   depends_on "go" => :build
 
